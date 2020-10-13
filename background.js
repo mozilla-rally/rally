fa1214ad3ede97d0765c2da0368892a9db46293b
Robert Helmer <rhelmer@rhelmer.org>
2020-10-13 14:39:49 -0700

    add license header

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 const ION_SIGNUP_URL = "https://mozilla-ion.github.io/ion-core-addon/";
 const CORE_ADDON_ID = "ion-core-addon@mozilla.org";
 
