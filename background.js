a96d663c633849116fc47a8c8c0d126bfa56c06a
Robert Helmer <rhelmer@rhelmer.org>
2020-10-13 09:23:41 -0700

    check for core add-on and open sign-up page if not present

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-/* eslint-disable no-undef */
+const ION_SIGNUP_URL = "https://mozilla-ion.github.io/ion-core-addon/";
+const CORE_ADDON_ID = "ion-core-addon@mozilla.org";
+
+async function checkIonCore() {
+  try {
+    const addon = await browser.management.get(CORE_ADDON_ID);
+  } catch (ex) {
+    console.debug("Core add-on not found, opening sign-up URL", ex);
+    await browser.tabs.create({ url: ION_SIGNUP_URL });
+  }
+}
+
 async function sendPing() {
   const currentDate = new Date();
 
@@ -14,6 +25,14 @@ async function sendPing() {
   // FIXME pass messages to core add-on
   throw new Error("not implemented");
 }
+
+checkIonCore()
+  .then((result) => {
+    console.debug("Done checking for core add-on");
+  })
+  .catch((ex) => {
+    console.error("Checking for core add-on failed, will re-try on startup");
+  });
 
 sendPing()
   .then((result) =>
