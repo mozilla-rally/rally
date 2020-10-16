b4da59e4cbbb10c28d0f8e46d11c5be1fd90d75b
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-10-16 13:27:30 +0200

    Add support for sending encrypted pings
    This adds the ion function to send encrypted
    pings through the core addon, using the study
    provided encryption keys.

--- background.js
+++ background.js
@@ -2,29 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-async function sendPing() {
-  const currentDate = new Date();
-
-  const payload = {
-    datetime: currentDate,
-  };
-
-  const options = {
-    schemaName: "debug",
-    schemaVersion: 1,
-  };
-
-  // FIXME pass messages to core add-on
-  throw new Error("not implemented");
-}
-
 let ion = new Ion();
-ion.initialize();
-
-sendPing()
-  .then((result) =>
-    console.info(
-      "Telemetry submitted, check about:telemetry archived ping data."
-    )
-  )
-  .catch((error) => console.error("Could not send ping:", error));
+ion.initialize(
+  // A sample key id used for encrypting data.
+  "sample-invalid-key-id",
+  // A sample *valid* JWK object for the encryption.
+  {
+    "kty":"EC",
+    "crv":"P-256",
+    "x":"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
+    "y":"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
+    "kid":"Public key used in JWS spec Appendix A.3 example"
+  }
+);
