5635f2bbfc0fa0e182237ba1573647c90ef10f64
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-01-08 12:01:29 +0100

    Remove unused OPTIONS_PAGE_PATH and add the license
    This makes the linter happier, because OPTIONS_PAGE_PATH
    was not being used.

--- src/ExampleModule.js
+++ src/ExampleModule.js
@@ -1,4 +1,6 @@
-const OPTIONS_PAGE_PATH = "public/index.html";
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 module.exports = {
     initialize() {
