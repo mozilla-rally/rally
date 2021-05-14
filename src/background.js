de43f1e5c97c0036d9b74f49275490fd8007f5f4
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2021-05-13 20:50:14 -0400

    Build system and documentation improvements

--- src/background.js
+++ src/background.js
@@ -2,15 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+// This is the main background script for the study template.
+// The build system will bundle dependencies into this script
+// and output the bundled scripr to dist/background.js.
+
+// Import the WebExtensions polyfill, for cross-browser compatibility.
+// Note that Rally and WebScience currently only support Firefox.
 import "webextension-polyfill";
 
+// Import the Rally API.
 import { Rally, runStates } from "@mozilla/rally";
 
+// Import the WebScience API.
 import * as webScience from "@mozilla/web-science";
 
 // Example: import a module.
 import {
-  initialize as exampleInitialize
+  initialize as exampleModuleInitialize
 } from './exampleModule';
 
 // Initialize the Rally API.
@@ -40,13 +48,39 @@ rally.initialize(
 ).then(resolve => {
   // The Rally API has been initialized.
   // Initialize the study and start it.
+
   // Example: initialize the example module.
-  exampleInitialize();
-  // Example: set a listener for WebScience page navigation events.
+  exampleModuleInitialize();
+
+  // Example: set a listener for WebScience page navigation events on
+  // *://*.mozilla.org/* pages. Note that the manifest origin
+  // permissions currently only include *://*.mozilla.org/*. You should
+  // update the manifest permissions as needed for your study.
   webScience.pageNavigation.onPageData.addListener(pageData => {
     console.log("WebScience page navigation event fired.");
+  }, {
+    matchPatterns: [ "*://*.mozilla.org/*" ]
   });
-}, reject =>{
+
+  // Example: register a content script for *://*.mozilla.org/* pages
+  // Note that the content script has the same relative path in dist/
+  // that it has in src/. The content script can include module
+  // dependencies (either your own modules or modules from npm), and
+  // they will be automatically bundled into the content script by
+  // the build system.
+  browser.contentScripts.register({
+    js: [ { file: "dist/exampleContentScript.content.js" } ],
+    matches: [ "*://*.mozilla.org/*" ]
+  });
+
+  // Example: launch a Web Worker, which can handle tasks on another
+  // thread. Note that the worker script has the same relative path in
+  // dist/ that it has in src/. The worker script can include module
+  // dependencies (either your own modules or modules from npm), and
+  // they will be automatically bundled into the worker script by the
+  // build system.
+  new Worker(browser.runtime.getURL("dist/exampleWorkerScript.worker.js"));
+}, reject => {
   // Do not start the study in this case. Something
   // went wrong.
 });
--- web-ext-config.js
+++ web-ext-config.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This is the web-ext configuration for the study template. It is
+// part of the build system, and you should not have to modify it.
+
 module.exports = {
   // Global options:
   verbose: true,
