9b62465085f11d4da747486b3004a373ca6fb6ff
Robert Helmer <rhelmer@rhelmer.org>
2021-07-27 08:56:31 -0700

    handle messages from website

--- extensions/sdk/dist/rally.js
+++ extensions/sdk/dist/rally.js
@@ -1408,6 +1408,7 @@ class Rally {
         // Set the initial state to paused, and register callback for future changes.
         this._state = exports.runStates.PAUSED;
         this._stateChangeCallback = stateChangeCallback;
+        browser$1.runtime.onMessageExternal.addListener((m, s) => this._handleWebMessage(m, s));
         this._promptSignUp().catch(err => console.error(err));
     }
     _promptSignUp() {
@@ -1493,6 +1494,77 @@ class Rally {
                 default:
                     throw new Error(`Rally._handleExternalMessage - unexpected message type ${message.type}`);
             }
+        });
+    }
+    /**
+   * Handles messages coming in from the external website.
+   *
+   * @param {Object} message
+   *        The payload of the message.
+   * @param {runtime.MessageSender} sender
+   *        An object containing information about who sent
+   *        the message.
+   * @returns {Promise} The response to the received message.
+   *          It can be resolved with a value that is sent to the
+   *          `sender` or rejected in case of errors.
+   */
+    _handleWebMessage(message, sender) {
+        console.log("Core - received web message", message, "from", sender);
+        try {
+            // Security check - only allow messages from our own site!
+            let platformURL = new URL("https://rally-web-spike.web.app/");
+            let senderURL = new URL(sender.url);
+            if (platformURL.origin != senderURL.origin) {
+                return Promise.reject(new Error(`Core - received message from unexpected URL ${sender.url}`));
+            }
+        }
+        catch (ex) {
+            return Promise.reject(new Error(`Core - cannot validate sender URL ${sender.url}`));
+        }
+        // ** IMPORTANT **
+        //
+        // The website should *NOT EVER* be trusted. Other addons could be
+        // injecting content scripts there too, impersonating the website
+        // and performing requests on its behalf.
+        //
+        // Do not ever add other features or messages here without thinking
+        // thoroughly of the implications: can the message be used to leak
+        // information out? Can it be used to mess with studies?
+        switch (message.type) {
+            case "web-check":
+                // The `web-check` message should be safe: any installed addon with
+                // the `management` privileges could check for the presence of the
+                // core addon and expose that to the web. By exposing this ourselves
+                // through content scripts enabled on our domain, we don't make things
+                // worse.
+                return Promise.resolve({
+                    type: "web-check-response",
+                    data: {}
+                });
+            case "complete-signup":
+                // The `open-rally` message should be safe: it exclusively opens
+                // the addon options page. It's a one-direction communication from the
+                // page, as no data gets exfiltrated or no message is reported back.
+                const signUpComplete = Boolean(this._completeSignUp(message.data));
+                return Promise.resolve({ type: "complete-signup", data: { signUpComplete } });
+            default:
+                return Promise.reject(new Error(`Core._handleWebMessage - unexpected message type "${message.type}"`));
+        }
+    }
+    _completeSignUp(authToken) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const signUpStorage = yield browser$1.storage.local.get("signUpComplete");
+            if (!("signUpComplete" in signUpStorage)) {
+                // Record sign-up complete.
+                yield browser$1.storage.local.set({ "signUpComplete": true });
+                // Store the auth token.
+                // TODO we should let Firebase handle it for us.
+                yield browser$1.storage.local.set({ authToken });
+            }
+            else {
+                console.warn("Sign-up is already complete.");
+            }
+            return true;
         });
     }
 }
