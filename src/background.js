a24fa4a38557463b4dca7c3269616182f256a175
Robert Helmer <rhelmer@rhelmer.org>
2021-09-10 20:29:23 -0700

    upgrade to latest rally-sdk

--- src/background.js
+++ src/background.js
@@ -8,7 +8,7 @@
 
 // Import the WebExtensions polyfill, for cross-browser compatibility.
 // Note that Rally and WebScience currently only support Firefox.
-import "webextension-polyfill";
+// import { browser } from "webextension-polyfill";
 
 // Import the Rally API.
 import { Rally, runStates } from "@mozilla/rally";
@@ -18,69 +18,87 @@ import * as webScience from "@mozilla/web-science";
 
 // Example: import a module.
 import {
-  initialize as exampleModuleInitialize
+  initialize as exampleModuleInitialize,
+  uninitialize as exampleModuleUninitialize
 } from './exampleModule';
 
-// Initialize the Rally API.
-const rally = new Rally();
-rally.initialize(
-  // A sample key id used for encrypting data.
-  "sample-invalid-key-id",
-  // A sample *valid* JWK object for the encryption.
-  {
-    "kty":"EC",
-    "crv":"P-256",
-    "x":"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
-    "y":"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
-    "kid":"Public key used in JWS spec Appendix A.3 example"
-  },
+// Initialize the Rally API.s
+const rally = new Rally(
   // The following constant is automatically provided by
   // the build system.
   __ENABLE_DEVELOPER_MODE__,
   // A sample callback with the study state.
-  (newState) => {
-    if (newState === runStates.RUNNING) {
-      console.log("The study can run.");
-    } else {
-      console.log("The study must stop.");
+  async function (newState) {
+    switch (newState) {
+      case (runStates.RUNNING):
+        console.log(`Study running with Rally ID: ${rally.rallyId}`);
+        // The Rally API has been initialized.
+        // Initialize the study and start it.
+
+        // Example: initialize the example module.
+        exampleModuleInitialize();
+
+        // Example: set a listener for WebScience page navigation events on
+        // *://*.mozilla.org/* pages. Note that the manifest origin
+        // permissions currently only include *://*.mozilla.org/*. You should
+        // update the manifest permissions as needed for your study.
+
+        this.pageDataListener = (pageData) => {
+          console.log(`WebScience page navigation event fired with page data: ${pageData}`);
+        };
+
+        webScience.pageNavigation.onPageData.addListener(this.pageDataListener, { matchPatterns: ["*://*.mozilla.org/*"] });
+
+        // Example: register a content script for *://*.mozilla.org/* pages
+        // Note that the content script has the same relative path in dist/
+        // that it has in src/. The content script can include module
+        // dependencies (either your own modules or modules from npm), and
+        // they will be automatically bundled into the content script by
+        // the build system.
+        this.contentScript = await browser.contentScripts.register({
+          js: [{ file: "dist/exampleContentScript.content.js" }],
+          matches: ["*://*.mozilla.org/*"]
+        });
+        // Example: launch a Web Worker, which can handle tasks on another
+        // thread. Note that the worker script has the same relative path in
+        // dist/ that it has in src/. The worker script can include module
+        // dependencies (either your own modules or modules from npm), and
+        // they will be automatically bundled into the worker script by the
+        // build system.
+
+        this.worker = new Worker("/dist/exampleWorkerScript.worker.js");
+
+        break;
+      case (runStates.PAUSED):
+        console.log(`Study paused with Rally ID: ${rally.rallyId}`);
+
+        // Take down all resources from run state.
+        exampleModuleUninitialize();
+        webScience.pageNavigation.onPageData.removeListener(this.pageDataListener);
+        this.contentScript.unregister();
+        this.worker.terminate();
+
+        break;
+      case (runStates.ENDED):
+        console.log(`Study ended with Rally ID: ${rally.rallyId}`);
+
+        break;
+      default:
+        throw new Error(`Unknown study state: ${newState}`);
     }
+  },
+  // The website hosting the Rally Web UI.
+  "http://localhost:3000",
+  // The Rally Study ID.
+  "exampleStudy1",
+  // The Firebase config.
+  {
+    "apiKey": "abc123",
+    "authDomain": "demo-rally.firebaseapp.com",
+    "projectId": "demo-rally",
+    "storageBucket": "demo-rally.appspot.com",
+    "messagingSenderId": "abc123",
+    "appId": "1:123:web:abc123",
+    "functionsHost": "http://localhost:5001"
   }
-).then(resolve => {
-  // The Rally API has been initialized.
-  // Initialize the study and start it.
-
-  // Example: initialize the example module.
-  exampleModuleInitialize();
-
-  // Example: set a listener for WebScience page navigation events on
-  // *://*.mozilla.org/* pages. Note that the manifest origin
-  // permissions currently only include *://*.mozilla.org/*. You should
-  // update the manifest permissions as needed for your study.
-  webScience.pageNavigation.onPageData.addListener(pageData => {
-    console.log("WebScience page navigation event fired.");
-  }, {
-    matchPatterns: [ "*://*.mozilla.org/*" ]
-  });
-
-  // Example: register a content script for *://*.mozilla.org/* pages
-  // Note that the content script has the same relative path in dist/
-  // that it has in src/. The content script can include module
-  // dependencies (either your own modules or modules from npm), and
-  // they will be automatically bundled into the content script by
-  // the build system.
-  browser.contentScripts.register({
-    js: [ { file: "dist/exampleContentScript.content.js" } ],
-    matches: [ "*://*.mozilla.org/*" ]
-  });
-
-  // Example: launch a Web Worker, which can handle tasks on another
-  // thread. Note that the worker script has the same relative path in
-  // dist/ that it has in src/. The worker script can include module
-  // dependencies (either your own modules or modules from npm), and
-  // they will be automatically bundled into the worker script by the
-  // build system.
-  new Worker(browser.runtime.getURL("dist/exampleWorkerScript.worker.js"));
-}, reject => {
-  // Do not start the study in this case. Something
-  // went wrong.
-});
+);
