f4df53d8f00d4f3ab922f19f0fa3a625c2d4ec44
Robert Helmer <rhelmer@rhelmer.org>
2021-09-25 14:38:34 -0700

    refactor out redundancy

--- public/options.js
+++ public/options.js
@@ -1,23 +1,25 @@
 const RUNNING = 0;
 const PAUSED = 1;
 
-browser.storage.onChanged.addListener((changes, area) => {
-    console.debug("received storage change:", changes, area);
-    if (changes.state) {
-        console.debug("changes state:", changes.state);
-        if (changes.state.newValue === RUNNING) {
-            console.debug("change state to running");
-            document.getElementById("status").textContent = "RUNNING";
-            document.getElementById("status").classList = ["bg-green-300"]
-        } else if (changes.state.newValue === PAUSED) {
-            console.debug("change state to paused");
-            document.getElementById("status").textContent = "PAUSED";
-            document.getElementById("status").classList = ["bg-red-300"];
-        } else {
-            console.error("Unknown change state:", changes.state);
-        }
+function changeState(state) {
+    if (state === RUNNING) {
+        document.getElementById("status").textContent = "RUNNING";
+        document.getElementById("toggleEnabled").checked = true;
+        document.getElementById("status").classList = ["bg-green-300"]
+    } else if (state === PAUSED || state === undefined) {
+        document.getElementById("status").textContent = "PAUSED";
+        document.getElementById("toggleEnabled").checked = false;
+        document.getElementById("status").classList = ["bg-red-300"];
+    } else {
+        console.error("Unknown state:", state);
     }
-});
+}
+
+// Update UI to current state.
+browser.storage.local.get("state").then(storage => changeState(storage.state));
+
+// Listen for state changes.
+browser.storage.onChanged.addListener((changes) => changeState(changes.state.newValue));
 
 document.getElementById("toggleEnabled").addEventListener("click", async event => {
     if (event.target.checked === true) {
@@ -27,9 +29,9 @@ document.getElementById("toggleEnabled").addEventListener("click", async event =
     }
 });
 
-document.getElementById("download").addEventListener("click", async event => {
+document.getElementById("download").addEventListener("click", async () => {
+    // Get all data
     const data = await browser.storage.local.get(null);
-    console.debug("Downloading data:", data);
 
     const dataUrl = (`data:application/json,${encodeURIComponent(JSON.stringify(data))}`);
 
@@ -37,18 +39,4 @@ document.getElementById("download").addEventListener("click", async event => {
     downloadLink.setAttribute("href", dataUrl);
     downloadLink.setAttribute("download", "rally-study-template.json");
     downloadLink.click();
-});
-
-browser.storage.local.get("state").then(storage => {
-    if (storage.state === PAUSED) {
-        document.getElementById("status").textContent = "PAUSED";
-        document.getElementById("toggleEnabled").checked = false;
-        document.getElementById("status").classList = ["bg-red-300"];
-    } else if (storage.state === RUNNING) {
-        document.getElementById("status").textContent = "RUNNING";
-        document.getElementById("toggleEnabled").checked = true;
-        document.getElementById("status").classList = ["bg-green-300"]
-    } else {
-        console.error("Unknown state:", storage.state);
-    }
 });
