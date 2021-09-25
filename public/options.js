fe20fcbb2f7da3677816d4e9a05578e94a3185d2
Robert Helmer <rhelmer@rhelmer.org>
2021-09-25 15:24:32 -0700

    dump as CSV instead of JSON, for use in Jupyter et al

--- public/options.js
+++ public/options.js
@@ -30,13 +30,53 @@ document.getElementById("toggleEnabled").addEventListener("click", async event =
 });
 
 document.getElementById("download").addEventListener("click", async () => {
-    // Get all data
+    // Get all data from local storage.
     const data = await browser.storage.local.get(null);
+    console.debug("Converting JSON to CSV:", data);
 
-    const dataUrl = (`data:application/json,${encodeURIComponent(JSON.stringify(data))}`);
+    // Extract all object keys to use as CSV headers.
+    const headerSet = new Set();
+    for (const [key, val] of Object.entries(data)) {
+        // Ignore bookeeping information.
+        if (!["initialized", "state"].includes(key)) {
+            for (const [header] of Object.entries(val)) {
+                headerSet.add(header);
+            }
+        }
+    }
+    const headers = Array.from(headerSet);
+
+    let csvData = "";
+
+    // Print one line with each header.
+    for (const [i, header] of headers.entries()) {
+        csvData += `${header}`;
+        if (i == headers.length - 1) {
+            csvData += `\n`;
+        } else {
+            csvData += `,`;
+        }
+    }
+
+    // Print the value for eachs measurement, in the same order as the headers on the first line.
+    for (const [key, val] of Object.entries(data)) {
+        // Ignore bookeeping information.
+        if (!["initialized", "state"].includes(key)) {
+            for (const [i, header] of headers.entries()) {
+                csvData += `${val[header]}`;
+                if (i == headers.length - 1) {
+                    csvData += `\n`;
+                } else {
+                    csvData += `,`;
+                }
+            }
+        }
+    }
+
+    const dataUrl = (`data:text/csv,${encodeURIComponent(csvData)}`);
 
     const downloadLink = document.getElementById("downloadLink");
     downloadLink.setAttribute("href", dataUrl);
-    downloadLink.setAttribute("download", "rally-study-template.json");
+    downloadLink.setAttribute("download", "rally-study-template.csv");
     downloadLink.click();
 });
