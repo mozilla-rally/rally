a3ad66a797db15178677e380387cea0b57e6d22e
Robert Helmer <rhelmer@rhelmer.org>
2021-09-26 14:02:13 -0700

    use local warc during tests

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -9,3 +9,5 @@ npm run test:integration:jest -- --test_browser=firefox --load_extension=true --
 
 # FIXME Chrome Headless mode does not support extensions, need to set up a display server if we want this to work.
 # npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
+
+kill $(jobs -p)
