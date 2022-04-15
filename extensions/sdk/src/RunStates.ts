8aba111a1e5c3fc8377923e942206a8a9543ba47
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-04-15 13:27:34 -0700

    Add strings values to RunStates.

--- extensions/sdk/src/RunStates.ts
+++ extensions/sdk/src/RunStates.ts
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 export enum RunStates {
-  Running,
-  Paused,
-  Ended
+  Running = "Running",
+  Paused = "Paused",
+  Ended = "Ended"
 }
