d70609879f7749407a0f73cc9fc199b7f881cb7f
Ashwin Agarwal <me@ashwinagarwal.com>
2022-05-10 23:20:55 -0400

    Remove moduleMapper from jest (workaround no longer needed)

--- web-platform/functions/jest.config.js
+++ web-platform/functions/jest.config.js
@@ -6,15 +6,6 @@ export default {
     },
   },
   resolver: "ts-jest-resolver",
-  moduleNameMapper: {
-    "^@mozilla/glean/node": "@mozilla/glean/dist/index/node.js",
-    "^@mozilla/glean/plugins/(.*)": "@mozilla/glean/dist/plugins/$1.js",
-    "^@mozilla/glean/uploader": "@mozilla/glean/dist/core/upload/uploader.js",
-    "^@mozilla/glean/private/metrics/(.*)":
-      "@mozilla/glean/dist/core/metrics/types/$1.js",
-    "^@mozilla/glean/private/ping":
-      "@mozilla/glean/dist/core/pings/ping_type.js",
-  },
   testRegex: "src(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: ["lib/", "node_modules/", "setupTests.js"],
   moduleFileExtensions: ["js", "ts", "tsx", "jsx", "json", "node"],
