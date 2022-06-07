a140f3cc21a825244a1cd5350d8e40f9fbe2a928
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-06-06 21:27:19 -0700

    Committing: https://github.com/mozilla-rally/rally-web-platform/commit/bc0c3819a6d69bdddea71238d5371c5038d29792 Except not bringing in the integration test change since it will be imported as part of svelte code.

--- web-platform/functions/src/studies.ts
+++ web-platform/functions/src/studies.ts
@@ -1,20 +1,33 @@
 export const studies = {
-  exampleStudy1: {
-    name: "Rally Test",
-    icons: {},
-    authors: {
-      name: "Rally Devs",
+  facebookPixelHunt: {
+    name: "Facebook Pixel Hunt",
+    icons: {
+      "32": "https://addons.mozilla.org/user-media/addon_icons/2732/2732005-32.png",
+      "64": "https://addons.mozilla.org/user-media/addon_icons/2732/2732005-64.png",
+      "128": "https://addons.mozilla.org/user-media/addon_icons/2732/2732005-128.png"
     },
-    version: "0.0.1",
-    addonId: "abc123",
-    studyId: "exampleStudy1",
-    downloadLink: "http://example.com",
-    endDate: "2021-10-27",
+    authors: {
+      name: "Mozilla Rally and the Markup",
+    },
+    version: "0.2.0",
+    firefoxAddonId: "facebook-pixel-hunt@rally.mozilla.org",
+    chromeExtensionId: "pbeklachfkbjddmglcccopmknmpfchdm",
+    studyId: "facebookPixelHunt",
+    downloadLink: "https://chrome.google.com/webstore/detail/facebook-pixel-hunt/pbeklachfkbjddmglcccopmknmpfchdm",
+    endDate: "2022-07-13",
     studyEnded: false,
     studyPaused: false,
-    description: "Test",
-    studyDetailsLink: "http://example.com",
-    dataCollectionDetails: ["Nothing"],
-    tags: ["test"],
+    description: "According to its own policy, Facebook may collect information about you across the web even if you don\u2019t have a Facebook account. One way Facebook performs this tracking is through a network of \u201Cpixels\u201D that may be installed on many of the sites you visit. By joining this study, you will help Rally and The Markup investigate and report on where Facebook is tracking you and what kind of information they are collecting.",
+    studyDetailsLink: "https://rally.mozilla.org/current-studies/facebook-pixel-hunt/index.html",
+    dataCollectionDetails: [
+      "The data sent to Facebook through Facebook Pixels",
+      "The URLs of the web pages you browse",
+      "The time you spend browsing pages",
+      "The presence of Facebook login cookies in your browser",
+      "A study survey that the user completes"
+    ],
+    tags: [
+      "social media"
+    ],
   },
 };
