320c780f565748a671f99612d5f51bf910cb3bcc
Robert Helmer <rhelmer@rhelmer.org>
2022-07-07 08:23:22 -0700

    temporarily disable integration tests

--- extensions/attention-stream/scripts/integration-test.sh
+++ extensions/attention-stream/scripts/integration-test.sh
@@ -16,5 +16,5 @@ XVFB_RUN() {
 #npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true  2>&1 | tee integration.log
 # NOTE Chrome Headless mode does not support extensions, so we use `xvfb` as the display server.
 # FIXME Chrome is crashing when running this under under Selenium on Linux only, disable for now.
-echo "Testing Chrome non-headless with extension"
-XVFB_RUN npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
+# echo "Testing Chrome non-headless with extension"
+# XVFB_RUN npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
