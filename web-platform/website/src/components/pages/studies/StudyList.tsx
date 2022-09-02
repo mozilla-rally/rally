5a0a151b8b9a27a3a73434477d7b40e7c9df94ef
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-09-01 19:20:40 -0700

    Filter out expired, paused and browser incompatible studies.
    Fixes: https://github.com/mozilla-rally/rally/issues/213

--- web-platform/website/src/components/pages/studies/StudyList.tsx
+++ web-platform/website/src/components/pages/studies/StudyList.tsx
@@ -1,3 +1,4 @@
+import { Study } from "@mozilla/rally-shared-types";
 import { HTMLAttributes } from "react";
 
 import { useStudies } from "../../../services/StudiesService";
@@ -16,11 +17,26 @@ export function StudyList({
 
   return (
     <div {...rest}>
-      {allStudies.map((s) => (
-        <StudyProvider study={s} key={s.studyId}>
-          <StudyCard />
-        </StudyProvider>
-      ))}
+      {allStudies.map(
+        (s) =>
+          isStudyValid(s) && (
+            <StudyProvider study={s} key={s.studyId}>
+              <StudyCard />
+            </StudyProvider>
+          )
+      )}
     </div>
   );
+}
+
+function isStudyValid(study: Study) {
+  const UA = window.navigator.userAgent;
+
+  const browserCompat =
+    (UA.includes("Firefox") &&
+      study.downloadLink &&
+      study.downloadLink.firefox) ||
+    (UA.includes("Chrome") && study.downloadLink && study.downloadLink.chrome);
+
+  return study && !study.studyEnded && !study.studyPaused && browserCompat;
 }
