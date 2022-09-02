75d659f24086815664d7ac1d6b40729406ebce5a
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-09-02 09:10:19 -0700

    Filter out expired, paused and browser incompatible studies. (#215)
    Fixes: https://github.com/mozilla-rally/rally/issues/213

--- web-platform/website/src/components/pages/studies/__tests__/StudyList.tests.tsx
+++ web-platform/website/src/components/pages/studies/__tests__/StudyList.tests.tsx
@@ -38,12 +38,24 @@ describe("StudyList tests", () => {
     expect(StudyCard).not.toBeCalled();
   });
 
-  it("renders all studies", () => {
+  it("renders all chrome studies", () => {
     (StudyProvider as jest.Mock).mockImplementation(({ children }) => children);
+
+    setUserAgent("Chrome");
 
     const allStudies = [
       { studyId: "studyId-1", data: "data-1" },
-      { studyId: "studyId-2", data: "data-2" },
+      {
+        studyId: "studyId-2",
+        data: "data-2",
+        downloadLink: { chrome: "some link" },
+      },
+      { studyId: "studyId-3", data: "data-2" },
+      {
+        studyId: "studyId-4",
+        data: "data-2",
+        downloadLink: { chrome: "some link" },
+      },
     ];
 
     (useStudies as jest.Mock).mockReturnValue({
@@ -56,12 +68,127 @@ describe("StudyList tests", () => {
     expect(document.querySelector(".hello-world")).toBeInTheDocument();
 
     allStudies.forEach((s) => {
+      if (!s.downloadLink || !s.downloadLink.chrome) {
+        return;
+      }
+
       expect(StudyProvider).toHaveBeenCalledWith(
         { study: s, children: expect.anything() },
         {}
       );
     });
 
-    expect(StudyCard).toHaveBeenCalledTimes(allStudies.length);
+    expect(StudyCard).toHaveBeenCalledTimes(2);
   });
+
+  it("renders all firefox studies", () => {
+    (StudyProvider as jest.Mock).mockImplementation(({ children }) => children);
+
+    setUserAgent("Firefox");
+
+    const allStudies = [
+      { studyId: "studyId-1", data: "data-1" },
+      {
+        studyId: "studyId-2",
+        data: "data-2",
+        downloadLink: { firefox: "some link" },
+      },
+      { studyId: "studyId-3", data: "data-2" },
+      {
+        studyId: "studyId-4",
+        data: "data-2",
+        downloadLink: { firefox: "some link" },
+      },
+    ];
+
+    (useStudies as jest.Mock).mockReturnValue({
+      isLoaded: true,
+      allStudies,
+    });
+
+    render(<StudyList className="hello-world" />);
+
+    expect(document.querySelector(".hello-world")).toBeInTheDocument();
+
+    allStudies.forEach((s) => {
+      if (!s.downloadLink || !s.downloadLink.firefox) {
+        return;
+      }
+
+      expect(StudyProvider).toHaveBeenCalledWith(
+        { study: s, children: expect.anything() },
+        {}
+      );
+    });
+
+    expect(StudyCard).toHaveBeenCalledTimes(2);
+  });
+
+  it("filters paused and expired studies", () => {
+    (StudyProvider as jest.Mock).mockImplementation(({ children }) => children);
+
+    setUserAgent("Chrome");
+
+    const allStudies = [
+      {
+        studyId: "studyId-1",
+        data: "data-1",
+        downloadLink: { chrome: "some link" },
+      },
+      {
+        studyId: "studyId-2",
+        data: "data-2",
+        downloadLink: { chrome: "some link" },
+        studyEnded: true,
+      },
+      {
+        studyId: "studyId-3",
+        data: "data-2",
+        downloadLink: { chrome: "some link" },
+        studyPaused: true,
+      },
+      {
+        studyId: "studyId-4",
+        data: "data-2",
+        downloadLink: { firefox: "some link" },
+      },
+    ];
+
+    (useStudies as jest.Mock).mockReturnValue({
+      isLoaded: true,
+      allStudies,
+    });
+
+    render(<StudyList className="hello-world" />);
+
+    expect(document.querySelector(".hello-world")).toBeInTheDocument();
+
+    expect(StudyProvider).toHaveBeenCalledWith(
+      { study: allStudies[0], children: expect.anything() },
+      {}
+    );
+
+    expect(StudyCard).toHaveBeenCalledTimes(1);
+  });
+
+  let userAgentStr = "";
+  function setUserAgent(userAgent: string) {
+    if (userAgent !== userAgentStr) {
+      userAgentStr = userAgent;
+
+      const userAgentProp = {
+        get: function () {
+          return userAgentStr;
+        },
+      };
+
+      try {
+        Object.defineProperty(window.navigator, "userAgent", userAgentProp);
+      } catch (e) {
+        window.navigator = Object.create(navigator, {
+          userAgent: userAgentProp,
+        });
+      }
+    }
+  }
 });
