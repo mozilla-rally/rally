733897f6218536c58a244fd100fea1bb7ea9e3f7
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-10-08 11:37:34 -0700

    Formatting tab -> space
    Fixed formatting by running npm run build
    Converting tabs to spaces

--- web-platform/website/src/styles/Fonts.ts
+++ web-platform/website/src/styles/Fonts.ts
@@ -1,58 +1,59 @@
-import { Colors } from "./Colors";
 import { style } from "typestyle";
 
+import { Colors } from "./Colors";
+
 export const FontsRaw = {
-	Headline: {
-		fontFamily: `"Zilla Slab", Inter, X-LocaleSpecific, sans-serif`,
-		fontWeight: 700,
-	},
-	Title: {
-		fontFamily: "Inter",
-		fontWeight: 700,
-	},
-	MediumBodySM: {
-		fontFamily: "Inter",
-		fontStyle: "normal",
-		fontWeight: "500",
-	},
-	Labels: {
-		color: Colors.ColorMarketingGray70,
-	},
+  Headline: {
+    fontFamily: `"Zilla Slab", Inter, X-LocaleSpecific, sans-serif`,
+    fontWeight: 700,
+  },
+  Title: {
+    fontFamily: "Inter",
+    fontWeight: 700,
+  },
+  MediumBodySM: {
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: "500",
+  },
+  Labels: {
+    color: Colors.ColorMarketingGray70,
+  },
 };
 
 export const Fonts = {
-	Headline: style(FontsRaw.Headline),
-	MediumBodySM: style(FontsRaw.MediumBodySM),
-	Labels: style(FontsRaw.Labels),
-	Title: style(FontsRaw.Title),
+  Headline: style(FontsRaw.Headline),
+  MediumBodySM: style(FontsRaw.MediumBodySM),
+  Labels: style(FontsRaw.Labels),
+  Title: style(FontsRaw.Title),
 };
 
 export const FontSizeRaw = {
-	xSmall: {
-		fontSize: `${12 / 16}rem`,
-	},
-	Small: {
-		fontSize: `${14 / 16}rem`,
-	},
-	Normal: {
-		fontSize: "1rem",
-	},
-	Large: {
-		fontSize: `${18 / 16}rem`,
-	},
-	xLarge: {
-		fontSize: `${20 / 16}rem`,
-	},
-	xxLarge: {
-		fontSize: `${24 / 16}rem`,
-	},
+  xSmall: {
+    fontSize: `${12 / 16}rem`,
+  },
+  Small: {
+    fontSize: `${14 / 16}rem`,
+  },
+  Normal: {
+    fontSize: "1rem",
+  },
+  Large: {
+    fontSize: `${18 / 16}rem`,
+  },
+  xLarge: {
+    fontSize: `${20 / 16}rem`,
+  },
+  xxLarge: {
+    fontSize: `${24 / 16}rem`,
+  },
 };
 
 export const FontSize = {
-	xSmall: style(FontSizeRaw.xSmall),
-	Small: style(FontSizeRaw.Small),
-	Normal: style(FontSizeRaw.Normal),
-	Large: style(FontSizeRaw.Large),
-	xLarge: style(FontSizeRaw.xLarge),
-	xxLarge: style(FontSizeRaw.xxLarge),
+  xSmall: style(FontSizeRaw.xSmall),
+  Small: style(FontSizeRaw.Small),
+  Normal: style(FontSizeRaw.Normal),
+  Large: style(FontSizeRaw.Large),
+  xLarge: style(FontSizeRaw.xLarge),
+  xxLarge: style(FontSizeRaw.xxLarge),
 };
