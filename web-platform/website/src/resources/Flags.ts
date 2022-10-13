ddcb231088107f2f2d5565d1d0b75dcd40b16614
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-10-13 09:40:30 -0700

    Setting default value of onboardingV2 flag to true. (#399)
    Cleanup shall be done in another followup PR.

--- web-platform/website/src/resources/Flags.ts
+++ web-platform/website/src/resources/Flags.ts
@@ -8,6 +8,6 @@ export const Flags: Record<string, Flag> = {
   onboardingV2: {
     name: "onboardingV2",
     description: "Enables new onboarding experience.",
-    defaultValue: false,
+    defaultValue: true,
   },
 };
