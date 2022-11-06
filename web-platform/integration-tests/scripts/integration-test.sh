9f508d3cb81a7caeb4b14a9dd03699e746862735
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-11-05 20:49:02 -0700

    Added emulator code to website and tested by running emulator Tested both manually as well as via integration test

--- web-platform/integration-tests/scripts/integration-test.sh
+++ web-platform/integration-tests/scripts/integration-test.sh
@@ -4,12 +4,13 @@ set -eo pipefail
 export PATH=$PATH:./node_modules/.bin
 
 # Start website
-pushd ../website && npm run dev && popd&
+pushd ../website && npm run dev:emulator && popd&
 
 echo "Testing Firefox"
 
 firebase emulators:exec \
   -c ../firebase/firebase.json \
+  --only auth,functions,firestore \
   --project demo-rally \
   "npm run load:data && npm run jest -- --browser_type=firefox --headless_mode=true" 2>&1 | tee integration.log
 
