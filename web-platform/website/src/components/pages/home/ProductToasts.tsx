4c9d7e904945ecee1f42159e2c4d04275b64171b
thomik-corp <85543848+thomik-corp@users.noreply.github.com>
2022-12-07 12:27:31 -0500

    Reverting some changes from merge from mainline

--- web-platform/website/src/components/pages/home/ProductToasts.tsx
+++ web-platform/website/src/components/pages/home/ProductToasts.tsx
@@ -6,39 +6,6 @@ import { EmailNotVerifiedToast } from "./EmailNotVerifiedToast";
 import { PrivacyToast } from "./PrivacyToast";
 
 export function ProductToasts() {
-  const [showEmailNotVerifiedToast, setShowEmailNotVerifiedToast] =
-    useState<boolean>(false);
-  const [showAddExtensionToast, setShowAddExtenionToast] =
-    useState<boolean>(false);
-  const [showAddPrivacyToast, setShowAddPrivacyToast] =
-    useState<boolean>(false);
-  const [showEmailDialog, setShowEmailDialog] = useState<boolean>(false);
-  const [showPrivacyDialog, setShowPrivacyDialog] = useState<boolean>(false);
-  const [browserType] = useState(detectBrowser());
-
-  const { isUserVerified, reloadUserVerification, sendEmailVerification } =
-    useAuthentication();
-  const { installedStudyIds, rallyExtensionStudy } = useStudies();
-  const { userDocument } = useUserDocument();
-
-  useEffect(() => {
-    reloadUserVerification();
-  }, []);
-
-  useEffect(() => {
-    setShowEmailNotVerifiedToast(!isUserVerified);
-  }, [isUserVerified]);
-
-  useEffect(() => {
-    setShowAddExtenionToast(
-      !installedStudyIds.includes(rallyExtensionStudy?.studyId || "")
-    );
-  }, [installedStudyIds, rallyExtensionStudy]);
-
-  useEffect(() => {
-    setShowAddPrivacyToast((userDocument && !userDocument.enrolled) ?? false);
-  }, [userDocument]);
-
   return (
     <Container className={styles.container}>
       <AddExtensionToast />
